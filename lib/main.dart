0203ab81f84bbb0bfed03023dc30c06da5fafdd8
nardo.li <ly4work@163.com>
2019-04-26 20:58:28 +0800

    [update]

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(() => runApp(MyApp));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '指尖',
+    );
+  }
 }
