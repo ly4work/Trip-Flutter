dad57249449eb7c86b78f3b4549b7c78052e18fc
ly4work <292931857@qq.com>
2019-04-30 17:03:01 +0800

    [update]

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-import 'package:chak/flutter_layout_page.dart';
-import 'package:chak/gesture_page.dart';
-import 'package:chak/home_page.dart';
-import 'package:chak/launch_use_page.dart';
-import 'package:chak/photo_app_page.dart';
 import 'package:chak/res_page.dart';
 import 'package:flutter/material.dart';
 
@@ -18,66 +13,7 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('主页'),
         ),
-        body: RouteNavigator(),
-      ),
-      routes: <String, WidgetBuilder>{
-        'layout': (BuildContext context) => LayoutPage(),
-        'home': (BuildContext context) => HomePage(),
-        'gesture': (BuildContext context) => GesturePage(),
-        'resource': (BuildContext context) => ResPage(),
-        'launch': (BuildContext context) => LaunchPage(),
-        'photo': (BuildContext context) => PhotoPage(),
-      },
-    );
-  }
-}
-
-class RouteNavigator extends StatefulWidget {
-  @override
-  _RouteNavigatorState createState() => new _RouteNavigatorState();
-}
-
-class _RouteNavigatorState extends State<RouteNavigator> {
-  bool byName = false;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          SwitchListTile(
-            title: Text('${byName ? "" : "不"}通过路由名跳转'),
-            value: byName,
-            onChanged: (value) {
-              setState(() {
-                byName = value;
-              });
-            },
-          ),
-          _item('layout 组件 Demo', LayoutPage(), 'layout'),
-          _item('HomePage 组件 Demo', HomePage(), 'home'),
-          _item('GesturePage 组件 Demo', GesturePage(), 'gesture'),
-          _item('ResPage 组件 Demo', ResPage(), 'resource'),
-          _item('LaunchPage 组件 Demo', LaunchPage(), 'launch'),
-          _item('PhotoPage 组件 Demo', PhotoPage(), 'photo'),
-        ],
-      ),
-    );
-  }
-
-  _item(String title, page, String routerName) {
-    return Container(
-      child: RaisedButton(
-        onPressed: () {
-          //  通过路由名跳转
-          if (byName) {
-            Navigator.pushNamed(context, routerName);
-          } else {
-            //  通过路由直接builder组件
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => page));
-          }
-        },
-        child: Text(title),
+        body: Image(width: 24.0, height: 24.0, image: AssetImage('assets/avatar.png')),
       ),
     );
   }
