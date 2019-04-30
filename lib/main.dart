915b145a2fbc7de1417b398ab6a21b5d6313575f
ly4work <292931857@qq.com>
2019-04-30 18:39:07 +0800

    [update]动画基础

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-void main() => runApp(MainApp());
+void main() => runApp(LogoApp());
 
-class MainApp extends StatelessWidget {
+//  动画 anmiation类
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => new _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+  AnimationStatus animationStatus;
+  double animationValue = 0;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    animation = Tween<double>(begin: 0, end: 300.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          animationValue = animation.value;
+        });
+      })
+      ..addStatusListener((AnimationStatus state) {
+        setState(() {
+          animationStatus = state;
+        });
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '指尖',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('主页'),
-        ),
-        body: Column(
+    return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
           children: <Widget>[
-            Stack(
-              children: <Widget>[
-                //  渐进式加载图片
-                Center(child: CircularProgressIndicator()),
-                Center(
-                  child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: 'http://www.devio.org/img/avatar.png'),
-                )
-              ],
+            GestureDetector(
+              onTap: () {
+                controller.reset();
+                controller.forward();
+              },
+              child: Text(
+                'Start',
+                textDirection: TextDirection.ltr,
+              ),
             ),
-            Stack(
-              children: <Widget>[
-                //  渐进式加载图片
-                Center(
-                    child: CachedNetworkImage(
-                      placeholder: (context, url) => CircularProgressIndicator(),
-                        imageUrl: 'https://picsum.photos/250?image=9')),
-              ],
+            Text('State: ' + animationStatus.toString(),
+                textDirection: TextDirection.ltr),
+            Text('Value: ' + animationValue.toString(),
+                textDirection: TextDirection.ltr),
+            Text('abc: ', textDirection: TextDirection.ltr),
+            Container(
+              height: animation.value,
+              width: animation.value,
+              child: FlutterLogo(),
             ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
