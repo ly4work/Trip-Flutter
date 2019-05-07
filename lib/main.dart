08c1e3bc0465eb4e7cbade0f3b6c7e75a893ccf9
ly4work <292931857@qq.com>
2019-05-07 17:55:20 +0800

    [update]学习http

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,74 @@
-import 'package:chak/navigator/tab_navigator.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as Http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
+Utf8Decoder utf8decoder = Utf8Decoder();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String showResult = '点了之后显示这个区域';
+
+  Future<CommonModel> fetchPost() async {
+    final response = await Http.get(
+        'http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final result = json.decode(utf8decoder.convert(response.bodyBytes));
+    print(CommonModel.fromJson(result));
+    return CommonModel.fromJson(result);
+  }
+
   @override
   Widget build(BuildContext context) {
-    const String homeTitle = '主页';
     return MaterialApp(
-      title: homeTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('http'),
+        ),
+        body: Column(
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                fetchPost().then((CommonModel value) {
+                  setState(() {
+                    showResult =
+                        '请求结果: \nhideAppBar: ${value.hideAppBar} \nicon: ${value.icon}  \ntitle: ${value.title} \nurl: ${value.url} \nstatusBarColor: ${value.statusBarColor}';
+                  });
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                child: Text('点我'),
+              ),
+            ),
+            Text(showResult)
+          ],
+        ),
       ),
-      home: TabNavigator(),
+    );
+  }
+}
+
+//  创建数据Model
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel(
+      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(
+      icon: json['icon'],
+      title: json['title'],
+      url: json['url'],
+      statusBarColor: json['statusBarColor'],
+      hideAppBar: json['hideAppBar'],
     );
   }
 }
