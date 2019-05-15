935baa23490197c623e30945828a6b2ece3fcfee
ly4work <292931857@qq.com>
2019-05-15 21:55:57 +0800

    [update] 本地存储

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as Http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
 void main() => runApp(MyApp());
@@ -11,15 +11,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String showResult = '点了之后显示这个区域';
-
-  Future<CommonModel> fetchPost() async {
-    final response = await Http.get(
-        'http://www.devio.org/io/flutter_app/json/test_common_model.json');
-    final result = json.decode(utf8decoder.convert(response.bodyBytes));
-    print(CommonModel.fromJson(result));
-    return CommonModel.fromJson(result);
-  }
+  String countString = '';
+  String localCount = '';
 
   @override
   Widget build(BuildContext context) {
@@ -28,63 +21,35 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('http'),
         ),
-        body: FutureBuilder<CommonModel>(
-          future: fetchPost(),
-          builder: (BuildContext context, AsyncSnapshot<CommonModel> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return new Text('Input a URL to start');
-                break;
-              case ConnectionState.waiting:
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-                break;
-              case ConnectionState.active:
-                return new Text('active ');
-                break;
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return new Text('${snapshot.error}',
-                      style: TextStyle(color: Colors.red));
-                } else {
-                  return new Column(
-                    children: <Widget>[
-                      Text('hideAppBar: ${snapshot.data.hideAppBar}'),
-                      Text('icon: ${snapshot.data.icon}'),
-                      Text('title: ${snapshot.data.title}'),
-                      Text('url: ${snapshot.data.url}'),
-                      Text('statusBarColor: ${snapshot.data.statusBarColor}'),
-                    ],
-                  );
-                }
-                break;
-              default:
-            }
-          },
-        ),
+        body: Column(children: <Widget>[
+          RaisedButton(
+            child: Text('increment'),
+            onPressed: _incrementCoutner,
+          ),
+          RaisedButton(
+            onPressed: _getCounter,
+            child: Text('get count'),
+          ),
+          Text(countString)
+        ]),
       ),
     );
   }
-}
 
-//  创建数据Model
-class CommonModel {
-  final String icon;
-  final String title;
-  final String url;
-  final String statusBarColor;
-  final bool hideAppBar;
+  _incrementCoutner() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      countString = countString + ' 1';
+    });
 
-  CommonModel(
-      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
-  factory CommonModel.fromJson(Map<String, dynamic> json) {
-    return CommonModel(
-      icon: json['icon'],
-      title: json['title'],
-      url: json['url'],
-      statusBarColor: json['statusBarColor'],
-      hideAppBar: json['hideAppBar'],
-    );
+    int counter = (prefs.getInt('counter') ?? 0) + 1;
+    await prefs.setInt('counter', counter);
+  }
+
+  _getCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      localCount = prefs.getInt('counter').toString();
+    });
   }
 }
