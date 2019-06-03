7a4966ddee3201c2bdeca42d7c861c575e1467b4
nardo.li <ly4work@163.com>
2019-06-04 00:19:52 +0800

    [update]编写model

---
+++ lib/model/config_model.dart
@@ -0,0 +1,8 @@
+// String searchUrl	String	Nullable
+class ConfigModel {
+  final String searchUrl;
+  ConfigModel({this.searchUrl});
+  factory ConfigModel.fromJson(Map<String , dynamic>json){
+    return ConfigModel(searchUrl: json['searchUrl']);
+  } 
+}
--- lib/model/home_model.dart
+++ lib/model/home_model.dart
@@ -1,3 +1,21 @@
+//   ConfigModel config	Object	NonNull
+// List<CommonModel> bannerList	Array	NonNull
+// List<CommonModel> localNavList	Array	NonNull
+// GridNavModel gridNav	Object	NonNull
+// List<CommonModel> subNavList	Array	NonNull
+// SalesBoxModel salesBox	Object	NonNull
+import 'package:chak/model/common_model.dart';
+import 'package:chak/model/config_model.dart';
+import 'package:chak/model/grid_nav_model.dart';
+import 'package:chak/model/sales_box_model.dart';
+
 class HomeModel {
-  
+  final ConfigModel config;
+  final List<CommonModel> bannerList;
+  final List<CommonModel> localNavList;
+  final GridNavModel gridNav;
+  final List<CommonModel> subNavList;
+  final SalesBoxModel salesBox;
+  HomeModel(this.config, this.bannerList, this.localNavList, this.gridNav,
+      this.subNavList, this.salesBox);
 }
